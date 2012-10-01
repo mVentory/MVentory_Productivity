db5103036543b5502efdccc13f9ff1ca57ef344d
anemets1@gmail.com <anemets1@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2012-10-01 17:27:58 +0000

    issue #266
    git-svn-id: https://mageventory.googlecode.com/svn/branches@745 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/local/ZetaPrints/MvProductivityPack/controllers/ImageController.php
+++ app/code/local/ZetaPrints/MvProductivityPack/controllers/ImageController.php
@@ -92,6 +92,25 @@ class ZetaPrints_MvProductivityPack_ImageController
 
     Mage::helper('MvProductivityPack')
       ->rotate($file, $angels[$rotate]);
+    
+    $imageProp1 = $request->get('imageWidth')>$request->get('imageHeight')?
+                  $request->get('imageWidth'):
+                  $request->get('imageHeight');  
+    
+    $_product = Mage::getModel('catalog/product')
+                  ->load($request->get('productId'));
+    $type = $request->get('thumb') == 'true'?'thumbnail':'image';
+    
+    $imageProp2 = null;
+    $imageFile = null;
+    if($type == 'thumbnail') {
+      $imageProp2 = $imageProp1;
+      $imageFile = $file;
+    }
+		
+    $image = Mage::helper('catalog/image')->init($_product, $type, $imageFile)
+               ->resize($imageProp1, $imageProp2)->__toString();
+    echo Zend_Json::encode(array('image'=>$image));
 
     return Zend_Json::encode(true);
   }
@@ -113,6 +132,17 @@ class ZetaPrints_MvProductivityPack_ImageController
 
     Mage::helper('MvProductivityPack')
       ->remove($file, $productId);
+      
+    if($request->get('thumb') != 'true') {
+      $imageProp = $request->get('imageWidth')>$request->get('imageHeight')?
+                   $request->get('imageWidth'):
+                   $request->get('imageHeight');
+      $_product = Mage::getModel('catalog/product')
+                    ->load($request->get('productId'));
+      $image = Mage::helper('catalog/image')->init($_product, 'image')
+               ->resize($imageProp)->__toString();
+      echo Zend_Json::encode(array('image'=>$image));
+    }
 
     return Zend_Json::encode(true);
   }
@@ -123,10 +153,11 @@ class ZetaPrints_MvProductivityPack_ImageController
 
     $request = $this->getRequest();
 
-    if (!($request->has('file') && $request->has('product')))
+    if (!($request->has('thumbImage') 
+        && $request->has('mainImage') && $request->has('product')))
       return;
 
-    $file = $request->get('file');
+    $file = $request->get('thumbImage');
     $productId = (int) $request->get('product');
 
     if (!($file && $productId >= 0))
@@ -134,7 +165,31 @@ class ZetaPrints_MvProductivityPack_ImageController
 
     Mage::helper('MvProductivityPack')
       ->setMainImage($file, $productId);
-
+      
+    $imageThumbProp = $request->get('imageThumbWidth') > $request->get('imageThumbHeight')?
+                  $request->get('imageThumbWidth'):
+                  $request->get('imageThumbHeight');
+                  
+    $imageProp = $request->get('imageWidth') > $request->get('imageHeight')?
+                  $request->get('imageWidth'):
+                  $request->get('imageHeight');  
+    
+    $_product = Mage::getModel('catalog/product')
+                  ->load($request->get('productId'));
+    		
+    $thumbImage = Mage::helper('catalog/image')
+                    ->init($_product, 'thumbnail', $request->get('mainImage'))
+                    ->resize($imageThumbProp, $imageThumbProp)
+                    ->__toString();
+               
+    $mainImage = Mage::helper('catalog/image')
+                   ->init($_product, 'image', $request->get('thumbImage'))
+                   ->resize($imageProp)
+                   ->__toString();
+      
+    echo Zend_Json::encode(array('thumbImage'=>$thumbImage,
+                                 'mainImage'=>$mainImage));
+                                 
     return Zend_Json::encode(true);
   }
 }
--- skin/frontend/default/default/js/mvproductivitypack/image_edit.js
+++ skin/frontend/default/default/js/mvproductivitypack/image_edit.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function ($) {
   var $form = $('#product_addtocart_form');
   var $menus = $form.find('.tm-image-editor-menu');
+  var $updImage;
 
   $menus
     .parent()
@@ -19,8 +20,8 @@ jQuery(document).ready(function ($) {
     })
     .on('mouseleave', function () {
       $(this)
-          .children('.tm-image-editor-menu')
-          .hide();
+        .children('.tm-image-editor-menu')
+        .hide();
     });
 
   $menus
@@ -40,94 +41,149 @@ jQuery(document).ready(function ($) {
     .on('click', set_main_button_click_handler);
 
   function rotate_image (file, rotate, complete) {
+    
+    var imageWidth = $updImage.width();
+    var imageHeight = $updImage.height();
+    
+    var productId = $form.find('input[name="product"]').val();
+    
+    if($updImage.parent().parent().is('li')) {
+      var thumb = true;
+    } else {
+      var thumb = false;
+    }
+    
     $.ajax({
       url: _tm_image_editor_rotate_url,
       type: 'POST',
       dataType: 'json',
-      data: { file: file, rotate:  rotate},
+      data: { file: file, rotate:  rotate, 
+              imageWidth: imageWidth, imageHeight: imageHeight, 
+              thumb: thumb, productId: productId},
       error: function (jqXHR, status, errorThrown) {
-        alert(status);
+        var currentBorder = $updImage.css('border');
+        $updImage.css('border','1px solid red');
+        setTimeout(function(){$updImage.css('border',currentBorder)}, 5000);
+        alert('Product ID: ' + productId + '\nImage file: ' + file + '\nStatus: ' + status);
       },
-      /*success: function (data, status, jqXHR) {
-        console.log(data);
-      },*/
+      success: function (data, status, jqXHR) {
+        $updImage.prop('src', data.image);
+      },
       complete: complete
     });
   }
 
   function remove_image (file, product_id, complete) {
+    if($updImage.parent().parent().is('li')) {
+      var thumb = true;
+    } else {
+      var thumb = false;
+    }
+    var imageWidth = $updImage.width();
+    var imageHeight = $updImage.height();
+    
     $.ajax({
       url: _tm_image_editor_remove_url,
       type: 'POST',
       dataType: 'json',
-      data: { file: file, product: product_id },
+      data: { file: file, product: product_id , thumb: thumb,
+              imageWidth: imageWidth, imageHeight: imageHeight},
       error: function (jqXHR, status, errorThrown) {
-        alert(status);
+        var currentBorder = $updImage.css('border');
+        $updImage.css('border','1px solid red');
+        setTimeout(function(){$updImage.css('border',currentBorder)}, 5000);
+        alert('Product ID: ' + productId + '\nImage file: ' + file + '\nStatus: ' + status);
       },
-      /*success: function (data, status, jqXHR) {
-        console.log(data);
-      },*/
+      success: function (data, status, jqXHR) {
+        //console.log(data);
+        if(data) {
+          $updImage
+            .prop('src', data.image);
+        }
+        if(thumb) {
+          $updImage.remove();
+        }
+      },
       complete: complete
     });
   }
 
-  function set_main_image (file, product_id, complete) {
+  function set_main_image (thumbImage, mainImage, product_id, complete) {
+    var $img = $form.find('.product-image img')
+
+    var $thumb = $form
+                   .find('img')
+                   .filter('[src$="' + thumbImage + '"]')
+                   
+    var imageThumbWidth = $thumb.width();
+    var imageThumbHeight = $thumb.height();
+    
+    var imageWidth = $img.width();
+    var imageHeight = $img.height();
+    
     $.ajax({
       url: _tm_image_editor_setmain_url,
       type: 'POST',
       dataType: 'json',
-      data: { file: file, product: product_id },
+      data: { product: product_id, thumbImage: thumbImage, mainImage: mainImage,
+              imageThumbWidth: imageThumbWidth, imageThumbHeight: imageThumbHeight,
+              imageWidth: imageWidth, imageHeight: imageHeight  },
       error: function (jqXHR, status, errorThrown) {
-        alert(status);
+        alert('Product ID: ' + productId + '\nImage file: ' + file + '\nStatus: ' + status);
       },
-      /*success: function (data, status, jqXHR) {
-        console.log(data);
-      },*/
+      success: function (data, status, jqXHR) {
+        //console.log(data);  
+        $thumb.prop('src', data.thumbImage);
+        $img.prop('src', data.mainImage);
+        $thumb.css('opacity', '1');
+        $img.css('opacity', '1');
+      },
       complete: complete
     });
   }
 
   function rotate_button_click_handler (event) {
     var $this = $(this);
-
+    /*
     $this
       .parent()
       .children('.rotate-image')
       .off('click', rotate_button_click_handler);
-
+    */
+    $this
+      .parent()
+      .hide();
     event.preventDefault();
 
     var image = $this
                   .parent()
                   .children('input')
                   .val();
-
+    $updImage = $this.parent().parent().find('img');
+    $updImage.css('opacity', '0.5');
+    
+    $updImage.parent().append('<div class="image-editor-loader"></div>');
+    if(!$updImage.parent().parent().is('li')) {
+      $('.image-editor-loader').show();
+    }
+    
     var rotate = event.data.rotate;
-
+    
     rotate_image(image, rotate, function () {
       //$this.on('click', { rotate: rotate }, rotate_button_click_handler);
-
-      $this
-        .parent()
-        .remove();
-    });
-
-    var $imgs = $form
-                  .find('img')
-                  .filter('[src$="' + image + '"]');
-
-    if (rotate == 'left')
-      $imgs.addClass('rotate-90');
-    else
-      $imgs.addClass('rotate90');
-
+      $updImage.attr('src', $updImage.prop('src').split('?')[0]+'?t='+ new Date().getTime());
+      $updImage.parent('a').prop('href', '/media/catalog/product' + image);
+      $updImage.css('opacity', '1');
+      $('.image-editor-loader').remove();
+    });              
+    
     return false;
   }
 
   function remove_button_click_handler (event) {
     var $this = $(this);
 
-    $this.off('click', remove_button_click_handler);
+    /*$this.off('click', remove_button_click_handler);*/
 
     event.preventDefault();
 
@@ -139,24 +195,21 @@ jQuery(document).ready(function ($) {
     var product_id = $form
                        .find('input[name="product"]')
                        .val();
-
+                       
+    $updImage = $this.parent().parent().find('img');
+    
     remove_image(image, product_id, function () {
       $this
         .parent()
-        .remove();
+        .hide();
     });
-
-    $form
-      .find('img')
-      .filter('[src$="' + image + '"]')
-      .remove();
   }
 
   
   function set_main_button_click_handler (event) {
     var $this = $(this);
 
-    $this.off('click', set_main_button_click_handler);
+    /*$this.off('click', set_main_button_click_handler);*/
 
     event.preventDefault();
 
@@ -168,27 +221,38 @@ jQuery(document).ready(function ($) {
     var product_id = $form
                        .find('input[name="product"]')
                        .val();
+                       
+    var $main_image_input = $form
+                              .find('.product-image')
+                              .parent()
+                              .find('input[name="_tm_image_file"]');
+                              
+    var mainImage = $main_image_input.val();
+    
+    var $mainImage = $form.find('.product-image img')
+    var $thumbImage = $form
+                        .find('img')
+                        .filter('[src$="' + image + '"]')
 
-    set_main_image(image, product_id, function () {
+    $mainImage.css('opacity', '0.5');
+    $thumbImage.css('opacity', '0.5');
+    
+    $mainImage.parent().append('<div class="image-editor-loader"></div>');
+    $('.image-editor-loader').show();
+
+    set_main_image(image, mainImage, product_id, function () {  
+      $('.image-editor-loader').remove();
+      /*
       $this
         .parent()
         .remove();
+      */
     });
 
-    var $img = $form.find('.product-image img')
-
-    var image_url = $img.prop('src');
-
-    var $thumb = $form
-                   .find('img')
-                   .filter('[src$="' + image + '"]')
-
-    $thumb
-      .width($thumb.width())
-      .prop('src', image_url);
-
-    $img
-      .width($img.width())
-      .prop('src', '/media/catalog/product/' + image);
+    $thumbImage.parent('a').prop('href', '/media/catalog/product' + $main_image_input.val());
+    $mainImage.parent('a').prop('href', '/media/catalog/product' + image);
+    
+    $this.parent().children('input').val($main_image_input.val());  
+    $main_image_input.val(image);
   }
 });
