d6ae051c8bdc56745dcdc4db8c20541f1020d194
anemets1@gmail.com <anemets1@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2012-10-01 17:33:10 +0000

    updates after Anatoly's comments for r685 (issue #252)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@746 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/local/ZetaPrints/MvProductivityPack/controllers/ImageController.php
+++ app/code/local/ZetaPrints/MvProductivityPack/controllers/ImageController.php
@@ -19,60 +19,6 @@ class ZetaPrints_MvProductivityPack_ImageController
     return Mage::registry('is_admin_logged') === true;
   }
 
-  public function getAction () {
-    $request = $this->getRequest();
-
-    if (!$request->has('file'))
-        return;
-
-    $fileName = $request->get('file');
-
-    $dispretionPath
-                  = Mage_Core_Model_File_Uploader::getDispretionPath($fileName);
-
-    $fileName = $dispretionPath . DS . $fileName;
-
-    $media = Mage::getModel('catalog/product_media_config');
-
-    if (!file_exists($media->getMediaPath($fileName)))
-      return;
-
-    $tokens = explode('.', $fileName);
-
-    $type = $tokens[count($tokens) - 1];
-
-    if ($type == 'jpg')
-      $type = 'jpeg';
-
-    $width = $request->has('width') && is_numeric($request->get('width'))
-               ? (int) $request->get('width') : null;
-
-    $height = $request->has('height') && is_numeric($request->get('height'))
-                  ? (int) $request->get('height') : null;
-
-    if ($width || $height) {
-      $image = Mage::getModel('catalog/product_image');
-
-      $image
-        ->setBaseFile($fileName)
-        ->setKeepFrame(false)
-        ->setWidth($width)
-        ->setHeight($height)
-        ->resize()
-        ->saveFile();
-
-      $fileName = $image->getNewFile();
-    }
-
-    $this
-      ->getResponse()
-      ->setHeader('Pragma', '', true)
-      ->setHeader('Expires', '', true)
-      ->setHeader('Content-Type', 'image/' . $type , true)
-      ->setHeader('Content-Length', filesize($fileName), true)
-      ->setBody(file_get_contents($fileName));
-  }
-
   public function rotateAction () {
     if (!$this->_isAdmin())
       return;
