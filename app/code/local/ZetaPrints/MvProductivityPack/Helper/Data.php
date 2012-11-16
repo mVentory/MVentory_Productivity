8f48c44aecd288ef5c2f916c409cd88cc2638adc
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2012-11-16 19:21:26 +0000

    (Productivity) Re-format code; remove trailing spaces (issue #322)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@925 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/local/ZetaPrints/MvProductivityPack/Helper/Data.php
+++ app/code/local/ZetaPrints/MvProductivityPack/Helper/Data.php
@@ -1,150 +1,150 @@
 <?php
-class ZetaPrints_MvProductivityPack_Helper_Data extends Mage_Core_Helper_Abstract
-{
 
-	const ATTRIBUTE_CODE = 'media_gallery';
+class ZetaPrints_MvProductivityPack_Helper_Data
+  extends Mage_Core_Helper_Abstract {
 
-	public function rotate ($file, $angle) {
-			$media = Mage::getModel('catalog/product_media_config');
+  const ATTRIBUTE_CODE = 'media_gallery';
 
-		if (!file_exists($media->getMediaPath($file)))
-			return;
+  public function rotate ($file, $angle) {
+    $media = Mage::getModel('catalog/product_media_config');
 
-		$image = Mage::getModel('catalog/product_image');
+    if (!file_exists($media->getMediaPath($file)))
+      return;
 
-		$image
-		  ->setBaseFile($file)
-		  //->setNewFile($image->getBaseFile())
-		  ->setQuality(100)
-		  ->setKeepFrame(false)
-		  ->rotate($angle)
-		  ->saveFile();
+    $image = Mage::getModel('catalog/product_image');
 
-		return $image->getNewFile();
-	}
+    $image
+      ->setBaseFile($file)
+      //->setNewFile($image->getBaseFile())
+      ->setQuality(100)
+      ->setKeepFrame(false)
+      ->rotate($angle)
+      ->saveFile();
 
-	public function remove ($file, $productId) {
-		Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+    return $image->getNewFile();
+  }
 
-		$product = Mage::getModel('catalog/product')->load($productId);
-
-		if (!$product->getId())
-			return;
-
-		$attributes = $product
-			->getTypeInstance(true)
-			->getSetAttributes($product);
-
-		if (!isset($attributes[self::ATTRIBUTE_CODE]))
-			return;
-
-		$gallery = $attributes[self::ATTRIBUTE_CODE];
-
-		if (!$gallery->getBackend()->getImage($product, $file))
-			return;
-
-		$gallery
-			->getBackend()
-			->removeImage($product, $file);
-
-		try {
-			$product->save();
-		} catch (Mage_Core_Exception $e) {
-			return;
-		}
-
-		return true;
-	}
-
-	public function setMainImage ($file, $productId) {
-		Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
-
-		$product = Mage::getModel('catalog/product')->load($productId);
-
-		if (!$product->getId())
-			return;
-
-		$attributes = $product
-						->getTypeInstance(true)
-						->getSetAttributes($product);
-
-		if (!isset($attributes[self::ATTRIBUTE_CODE]))
-			return;
-
-		$gallery = $attributes[self::ATTRIBUTE_CODE];
-
-		if (!$gallery->getBackend()->getImage($product, $file))
-			return;
-
-		$currentImage = $product->getImage();
-
-		if ($currentImage)
-		  $gallery
-			->getBackend()
-			->updateImage($product, $currentImage, array('exclude' => false));
-
-		$gallery
-		  ->getBackend()
-		  ->updateImage($product, $file, array('exclude' => true));
-
-		$gallery
-		  ->getBackend()
-		  ->setMediaAttribute($product,
-							  array('image', 'small_image', 'thumbnail'),
-							  $file);
-
-		try {
-			$product->save();
-		} catch (Mage_Core_Exception $e) {
-			Mage::log($product);
-			return;
-		}
-
-		return true;
-	}
-  
-  public function updateImageInGallery($oldFile, $newFile, $productId, 
-                                       $mediaAttribute = null, $move = true, 
-                                       $exclude = false) {
+  public function remove ($file, $productId) {
     Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
-      
+
     $product = Mage::getModel('catalog/product')->load($productId);
 
-  	if (!$product->getId())
+    if (!$product->getId())
       return;
-      
-    $attributes = $product->getTypeInstance(true)->getSetAttributes($product);
-    
+
+    $attributes = $product
+                    ->getTypeInstance(true)
+                    ->getSetAttributes($product);
+
     if (!isset($attributes[self::ATTRIBUTE_CODE]))
       return;
-      
+
+    $gallery = $attributes[self::ATTRIBUTE_CODE];
+
+    if (!$gallery->getBackend()->getImage($product, $file))
+      return;
+
+    $gallery
+      ->getBackend()
+      ->removeImage($product, $file);
+
+    try {
+      $product->save();
+    } catch (Mage_Core_Exception $e) {
+      return;
+    }
+
+    return true;
+  }
+
+  public function setMainImage ($file, $productId) {
+    Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+    $product = Mage::getModel('catalog/product')->load($productId);
+
+    if (!$product->getId())
+      return;
+
+    $attributes = $product
+                    ->getTypeInstance(true)
+                    ->getSetAttributes($product);
+
+    if (!isset($attributes[self::ATTRIBUTE_CODE]))
+      return;
+
+    $gallery = $attributes[self::ATTRIBUTE_CODE];
+
+    if (!$gallery->getBackend()->getImage($product, $file))
+      return;
+
+    $currentImage = $product->getImage();
+
+    if ($currentImage)
+      $gallery
+        ->getBackend()
+        ->updateImage($product, $currentImage, array('exclude' => false));
+
+    $gallery
+      ->getBackend()
+      ->updateImage($product, $file, array('exclude' => true));
+
+    $gallery
+      ->getBackend()
+      ->setMediaAttribute($product,
+                array('image', 'small_image', 'thumbnail'),
+                $file);
+
+    try {
+      $product->save();
+    } catch (Mage_Core_Exception $e) {
+      Mage::log($product);
+      return;
+    }
+
+    return true;
+  }
+
+  public function updateImageInGallery ($oldFile, $newFile, $productId,
+                                        $mediaAttribute = null, $move = true,
+                                        $exclude = false) {
+
+    Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+    $product = Mage::getModel('catalog/product')->load($productId);
+
+    if (!$product->getId())
+      return;
+
+    $attributes = $product
+                    ->getTypeInstance(true)
+                    ->getSetAttributes($product);
+
+    if (!isset($attributes[self::ATTRIBUTE_CODE]))
+      return;
+
     $mediaGalleryAttribute = $attributes[self::ATTRIBUTE_CODE];
-      
+
     $mediaGalleryAttribute
       ->getBackend()
       ->removeImage($product, $oldFile);
-                
+
     $file = $mediaGalleryAttribute
               ->getBackend()
-              ->addImage($product, 
-                         $newFile, 
-                         $mediaAttribute, 
-                         $move, 
-                         $exclude);
-   
+              ->addImage($product, $newFile, $mediaAttribute, $move, $exclude);
+
     $product->save();
-      
+
     return $file;
   }
-	
+
   public function isAdminLogged () {
-		return Mage::registry('is_admin_logged') === true;
-	}
-  
-  public function saveAdminState() {
+    return Mage::registry('is_admin_logged') === true;
+  }
+
+  public function saveAdminState () {
     Mage::getSingleton('core/session', array('name' => 'adminhtml'))->start();
 
-		Mage::register('is_admin_logged', 
+    Mage::register('is_admin_logged',
                    Mage::getSingleton('admin/session')->isLoggedIn());
   }
 }
