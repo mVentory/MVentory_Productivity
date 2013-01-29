c865204d6fc76d47d4410479c14908eda249612c
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-01-29 13:51:37 +0000

    (Productivity) Replace tabs with spaces; remove trailing spaces
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1182 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/local/ZetaPrints/MvProductivityPack/controllers/CategoryController.php
+++ app/code/local/ZetaPrints/MvProductivityPack/controllers/CategoryController.php
@@ -9,205 +9,205 @@
 require_once("Mage/Catalog/controllers/CategoryController.php");
 class ZetaPrints_MvProductivityPack_CategoryController extends Mage_Catalog_CategoryController
 {
-	protected $_mediaNamespace = 'http://search.yahoo.com/mrss/';
-	protected $_rssGetVariable = 'rss';
-	protected $_thumbnailSizeGetVariable = 'thumbnail_size';
-	protected $_fullImageSizeGetVariable = 'fullimage_size';
-	
-	/**
-	 * Below preDispatch method will check whether admin is logged in or not on admin side and add the value in 
-	 * registry 
-	 */
-	public function preDispatch () {
-		Mage::helper('MvProductivityPack')->saveAdminState();
+  protected $_mediaNamespace = 'http://search.yahoo.com/mrss/';
+  protected $_rssGetVariable = 'rss';
+  protected $_thumbnailSizeGetVariable = 'thumbnail_size';
+  protected $_fullImageSizeGetVariable = 'fullimage_size';
 
-		return parent::preDispatch();
+  /**
+   * Below preDispatch method will check whether admin is logged in or not on admin side and add the value in
+   * registry
+   */
+  public function preDispatch () {
+    Mage::helper('MvProductivityPack')->saveAdminState();
 
-		return $this;
-	}
-	
-	private function removeqsvar($url, $varname) {
-		list($urlpart, $qspart) = array_pad(explode('?', $url), 2, '');
-		parse_str($qspart, $qsvars);
-		unset($qsvars[$varname]);
-		$newqs = http_build_query($qsvars);
-		return $urlpart . (strlen($newqs)>0?('?'.$newqs):"");
-	}
-	
-	/* Get a url pointing to the normal version of the products list page (not the rss one) */
-	private function getNoRssUrl()
-	{
-		$currentUrl = Mage::helper('core/url')->getCurrentUrl();
+    return parent::preDispatch();
 
-		$result = $this->removeqsvar($currentUrl, $this->_rssGetVariable);
-		$result = $this->removeqsvar($result, $this->_thumbnailSizeGetVariable);
-		$result = $this->removeqsvar($result, $this->_fullImageSizeGetVariable);
-		
-		return $result;
-	}
+    return $this;
+  }
 
-	private function getRssDescriptionHtml($product, $thumbnailUrl, $largeImageUrl)
-	{
-		return  
-		'<div>'."\n".
-			'<a class="mv-rss-preview" rel="'.$largeImageUrl.'" href="'.$product->getProductUrl().'" title="'.$product->getName().'">' ."\n".
-				'<img src="'.$thumbnailUrl.'" alt="'.$product->getName().'" title="'.$product->getName().'"/>' ."\n".
-			'</a>'."\n".
-			'<div class="mv-rss-price">'.Mage::helper('core')->currency($product->getPrice(),true,false).'</div>'."\n".
-		'</div>';
-	}
-	
-	private function formatXml($xml)
-	{
-		$dom = new DOMDocument('1.0');
-		$dom->preserveWhiteSpace = false;
-		$dom->formatOutput = true;
-		$dom->loadXML($xml);
-		
-		return $dom->saveXML();
-	}
-	
-	public function topAction()
-	{
-		$categoryModel = Mage::getModel('catalog/category');
-		$list = Mage::helper('catalog/category')->getStoreCategories();
-		 
-		$rssXml = new SimpleXMLElement('<rss/>');
-		$rssXml->addAttribute('version', '2.0');
-		
-		$pubDate = date("D, d M o G:i:s T",time());
-		
-		$channel = $rssXml->addChild('channel');
-		$channel->title = "Top categories per store";
-		$channel->addChild("description");
-		$channel->pubDate = $pubDate;
-		$channel->lastBuildDate = $pubDate;
-		$channel->generator = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB);
-		
-		foreach ($list as $cat)
-		{
-			$url = $categoryModel->setData($cat->getData())->getUrl();
-		 	
-			$item = $channel->addChild('item');
-			$item->title = $cat->getName();
-			$item->link = $url;
-		}
+  private function removeqsvar($url, $varname) {
+    list($urlpart, $qspart) = array_pad(explode('?', $url), 2, '');
+    parse_str($qspart, $qsvars);
+    unset($qsvars[$varname]);
+    $newqs = http_build_query($qsvars);
+    return $urlpart . (strlen($newqs)>0?('?'.$newqs):"");
+  }
 
-		$this->getResponse()->setBody($this->formatXml($rssXml->asXML()));
+  /* Get a url pointing to the normal version of the products list page (not the rss one) */
+  private function getNoRssUrl()
+  {
+    $currentUrl = Mage::helper('core/url')->getCurrentUrl();
 
-		$apiConfigCharset = Mage::getStoreConfig("api/config/charset");
-		$this->getResponse()->setHeader('Content-Type','application/rss+xml; charset='.$apiConfigCharset);
-	}
-	
-	public function viewAction()
-	{
-		parent::viewAction();
-		
-		if ($this->getRequest()->getParam($this->_rssGetVariable, 0) == 1)
-		{
-			/* Default image values */
-			$thumbnailWidth = 215;
-			$thumbnailHeight = 170;
-			
-			$fullImageWidth = 300;
-			$fullImageHeight = 300;
-			
-			$thumbnailSize = $this->getRequest()->getParam($this->_thumbnailSizeGetVariable);
-			$fullImageSize = $this->getRequest()->getParam($this->_fullImageSizeGetVariable);
-			
-			if (!is_null($thumbnailSize))
-			{
-				if (strcmp($thumbnailSize, "full") == 0)
-				{
-					$thumbnailWidth = null;
-					$thumbnailHeight = null;
-				}
-				else
-				{
-					$wh = explode("x", $thumbnailSize);
-					if (count($wh)==2) {
-						$thumbnailWidth = strlen($wh[0])>0?$wh[0]:null;
-						$thumbnailHeight = strlen($wh[1])>0?$wh[1]:null;
-					}
-				}
-			}
-			
-			if (!is_null($fullImageSize))
-			{
-				if (strcmp($fullImageSize, "full") == 0)
-				{
-					$fullImageWidth = null;
-					$fullImageHeight = null;
-				}
-				else
-				{
-					$wh = explode("x", $fullImageSize);
-					if (count($wh)==2) {
-						$fullImageWidth = strlen($wh[0])>0?$wh[0]:null;
-						$fullImageHeight = strlen($wh[1])>0?$wh[1]:null;
-					}
-				}
-			}
-			
-			$collection = $this->getLayout()->getBlock("product_list")->getLoadedProductCollection();
+    $result = $this->removeqsvar($currentUrl, $this->_rssGetVariable);
+    $result = $this->removeqsvar($result, $this->_thumbnailSizeGetVariable);
+    $result = $this->removeqsvar($result, $this->_fullImageSizeGetVariable);
 
-			$rssXml = new SimpleXMLElement('<rss xmlns:media="' . $this->_mediaNamespace . '"/>');
-			$rssXml->registerXPathNamespace('media', $this->_mediaNamespace); 
-			$rssXml->addAttribute('version', '2.0');
-			
-			$pubDate = date("D, d M o G:i:s T",time());
-			
-			$channel = $rssXml->addChild('channel');
-			$channel->title = $this->getLayout()->getBlock("head")->getTitle();
-			$channel->link = $this->getNoRssUrl();
-			$channel->addChild("description");
-			$channel->pubDate = $pubDate;
-			$channel->lastBuildDate = $pubDate;
-			$channel->generator = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB);
-			
-			foreach($collection as $product)
-			{
-				$image = Mage::helper('catalog/image')->init($product, 'small_image')->resize($thumbnailWidth, $thumbnailHeight);
-				$thumbnailUrl = "" . $image;
-				
-				$image = Mage::helper('catalog/image')->init($product, 'small_image')->resize($fullImageWidth, $fullImageHeight);
-				$largeImageUrl = "" . $image;
-				
-				$item = $channel->addChild('item');
-				$item->title = $product->getName();
-				$item->link = $product->getProductUrl();
-				$item->description = $this->getRssDescriptionHtml($product, $thumbnailUrl, $largeImageUrl);
-				$item->addChild("pubDate");
-				$item->addChild("author");
-				$item->addChild("guid");
-				$mediaContent = $item->addChild("content", "", $this->_mediaNamespace);
-				$item->addChild("title", htmlspecialchars($product->getName()), $this->_mediaNamespace);
-				$mediaThumbnail = $item->addChild("thumbnail", "", $this->_mediaNamespace);
+    return $result;
+  }
 
-				$mediaContent->addAttribute("url", $largeImageUrl);
-				$mediaThumbnail->addAttribute("url", $thumbnailUrl);
+  private function getRssDescriptionHtml($product, $thumbnailUrl, $largeImageUrl)
+  {
+    return
+    '<div>'."\n".
+      '<a class="mv-rss-preview" rel="'.$largeImageUrl.'" href="'.$product->getProductUrl().'" title="'.$product->getName().'">' ."\n".
+        '<img src="'.$thumbnailUrl.'" alt="'.$product->getName().'" title="'.$product->getName().'"/>' ."\n".
+      '</a>'."\n".
+      '<div class="mv-rss-price">'.Mage::helper('core')->currency($product->getPrice(),true,false).'</div>'."\n".
+    '</div>';
+  }
 
-				if (!is_null($fullImageWidth)) {
-					$mediaContent->addAttribute("width", $fullImageWidth);
-				}
-				if (!is_null($fullImageHeight)) {
-					$mediaContent->addAttribute("height", $fullImageHeight);
-				}
+  private function formatXml($xml)
+  {
+    $dom = new DOMDocument('1.0');
+    $dom->preserveWhiteSpace = false;
+    $dom->formatOutput = true;
+    $dom->loadXML($xml);
 
-				if (!is_null($thumbnailWidth)) {
-					$mediaThumbnail->addAttribute("width", $thumbnailWidth);
-				}
-				if (!is_null($thumbnailHeight)) {
-					$mediaThumbnail->addAttribute("height", $thumbnailHeight);
-				}
-			}
+    return $dom->saveXML();
+  }
 
-			$this->getResponse()->setBody($this->formatXml($rssXml->asXML()));
+  public function topAction()
+  {
+    $categoryModel = Mage::getModel('catalog/category');
+    $list = Mage::helper('catalog/category')->getStoreCategories();
 
-			$apiConfigCharset = Mage::getStoreConfig("api/config/charset");
-			$this->getResponse()->setHeader('Content-Type','application/rss+xml; charset='.$apiConfigCharset);
-		}
-	}
-	
+    $rssXml = new SimpleXMLElement('<rss/>');
+    $rssXml->addAttribute('version', '2.0');
+
+    $pubDate = date("D, d M o G:i:s T",time());
+
+    $channel = $rssXml->addChild('channel');
+    $channel->title = "Top categories per store";
+    $channel->addChild("description");
+    $channel->pubDate = $pubDate;
+    $channel->lastBuildDate = $pubDate;
+    $channel->generator = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB);
+
+    foreach ($list as $cat)
+    {
+      $url = $categoryModel->setData($cat->getData())->getUrl();
+
+      $item = $channel->addChild('item');
+      $item->title = $cat->getName();
+      $item->link = $url;
+    }
+
+    $this->getResponse()->setBody($this->formatXml($rssXml->asXML()));
+
+    $apiConfigCharset = Mage::getStoreConfig("api/config/charset");
+    $this->getResponse()->setHeader('Content-Type','application/rss+xml; charset='.$apiConfigCharset);
+  }
+
+  public function viewAction()
+  {
+    parent::viewAction();
+
+    if ($this->getRequest()->getParam($this->_rssGetVariable, 0) == 1)
+    {
+      /* Default image values */
+      $thumbnailWidth = 215;
+      $thumbnailHeight = 170;
+
+      $fullImageWidth = 300;
+      $fullImageHeight = 300;
+
+      $thumbnailSize = $this->getRequest()->getParam($this->_thumbnailSizeGetVariable);
+      $fullImageSize = $this->getRequest()->getParam($this->_fullImageSizeGetVariable);
+
+      if (!is_null($thumbnailSize))
+      {
+        if (strcmp($thumbnailSize, "full") == 0)
+        {
+          $thumbnailWidth = null;
+          $thumbnailHeight = null;
+        }
+        else
+        {
+          $wh = explode("x", $thumbnailSize);
+          if (count($wh)==2) {
+            $thumbnailWidth = strlen($wh[0])>0?$wh[0]:null;
+            $thumbnailHeight = strlen($wh[1])>0?$wh[1]:null;
+          }
+        }
+      }
+
+      if (!is_null($fullImageSize))
+      {
+        if (strcmp($fullImageSize, "full") == 0)
+        {
+          $fullImageWidth = null;
+          $fullImageHeight = null;
+        }
+        else
+        {
+          $wh = explode("x", $fullImageSize);
+          if (count($wh)==2) {
+            $fullImageWidth = strlen($wh[0])>0?$wh[0]:null;
+            $fullImageHeight = strlen($wh[1])>0?$wh[1]:null;
+          }
+        }
+      }
+
+      $collection = $this->getLayout()->getBlock("product_list")->getLoadedProductCollection();
+
+      $rssXml = new SimpleXMLElement('<rss xmlns:media="' . $this->_mediaNamespace . '"/>');
+      $rssXml->registerXPathNamespace('media', $this->_mediaNamespace);
+      $rssXml->addAttribute('version', '2.0');
+
+      $pubDate = date("D, d M o G:i:s T",time());
+
+      $channel = $rssXml->addChild('channel');
+      $channel->title = $this->getLayout()->getBlock("head")->getTitle();
+      $channel->link = $this->getNoRssUrl();
+      $channel->addChild("description");
+      $channel->pubDate = $pubDate;
+      $channel->lastBuildDate = $pubDate;
+      $channel->generator = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB);
+
+      foreach($collection as $product)
+      {
+        $image = Mage::helper('catalog/image')->init($product, 'small_image')->resize($thumbnailWidth, $thumbnailHeight);
+        $thumbnailUrl = "" . $image;
+
+        $image = Mage::helper('catalog/image')->init($product, 'small_image')->resize($fullImageWidth, $fullImageHeight);
+        $largeImageUrl = "" . $image;
+
+        $item = $channel->addChild('item');
+        $item->title = $product->getName();
+        $item->link = $product->getProductUrl();
+        $item->description = $this->getRssDescriptionHtml($product, $thumbnailUrl, $largeImageUrl);
+        $item->addChild("pubDate");
+        $item->addChild("author");
+        $item->addChild("guid");
+        $mediaContent = $item->addChild("content", "", $this->_mediaNamespace);
+        $item->addChild("title", htmlspecialchars($product->getName()), $this->_mediaNamespace);
+        $mediaThumbnail = $item->addChild("thumbnail", "", $this->_mediaNamespace);
+
+        $mediaContent->addAttribute("url", $largeImageUrl);
+        $mediaThumbnail->addAttribute("url", $thumbnailUrl);
+
+        if (!is_null($fullImageWidth)) {
+          $mediaContent->addAttribute("width", $fullImageWidth);
+        }
+        if (!is_null($fullImageHeight)) {
+          $mediaContent->addAttribute("height", $fullImageHeight);
+        }
+
+        if (!is_null($thumbnailWidth)) {
+          $mediaThumbnail->addAttribute("width", $thumbnailWidth);
+        }
+        if (!is_null($thumbnailHeight)) {
+          $mediaThumbnail->addAttribute("height", $thumbnailHeight);
+        }
+      }
+
+      $this->getResponse()->setBody($this->formatXml($rssXml->asXML()));
+
+      $apiConfigCharset = Mage::getStoreConfig("api/config/charset");
+      $this->getResponse()->setHeader('Content-Type','application/rss+xml; charset='.$apiConfigCharset);
+    }
+  }
+
 }
 
