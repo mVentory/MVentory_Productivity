2563fd843fd72e501d842ec2bf7044a40d0c6215
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-01-30 10:33:45 +0000

    (Productivity) Add feed for latest products (issue #385)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1194 805d05ed-e12b-7278-cd1c-88623764219b

---
+++ app/code/local/ZetaPrints/MvProductivityPack/Block/Rss/Product/Latest.php
@@ -0,0 +1,45 @@
+<?php
+
+class ZetaPrints_MvProductivityPack_Block_Rss_Product_Latest
+  extends Mage_Rss_Block_Abstract {
+
+  protected function _construct () {
+    parent::_construct();
+
+    $this
+      ->addData(array(
+        'cache_lifetime' => 1800,
+        'cache_tags' => array(Mage_Catalog_Model_Product::CACHE_TAG),
+      ));
+  }
+
+  /**
+   * Get Key pieces for caching block content
+   *
+   * @return array
+   */
+  public function getCacheKeyInfo () {
+    return array(
+      'PRODUCTIVITY_RSS_PRODUCT_LATEST',
+      $this->_getStoreId(),
+      Mage::getSingleton('customer/session')->getCustomerGroupId(),
+      $this->getProductsCount()
+    );
+  }
+
+  protected function _toHtml () {
+    $products = $this
+                  ->getLayout()
+                  ->createBlock('MvProductivityPack/product_latest')
+                  ->getProductCollection();
+
+    $data = array(
+      'title' => $this->__('Latest products'),
+      'link' => Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB)
+    );
+
+    return $this
+             ->helper('MvProductivityPack/rss')
+             ->generateFeedForProducts($products, $data);
+  }
+}
