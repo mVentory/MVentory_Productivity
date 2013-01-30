022707f824377cf1ce65093433cb0f8edbfbcebe
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-01-30 05:14:27 +0000

    (Productivity) Add link to RSS on category page (issue #385)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1188 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/local/ZetaPrints/MvProductivityPack/Model/Observer.php
+++ app/code/local/ZetaPrints/MvProductivityPack/Model/Observer.php
@@ -1,4 +1,21 @@
 <?php
 
 class ZetaPrints_MvProductivityPack_Model_Observer {
+  public function addCategoryFeed ($observer) {
+    $head = Mage::app()
+              ->getFrontController()
+              ->getAction()
+              ->getLayout()
+              ->getBlock('head');
+
+    if (!$head)
+      return;
+
+    $helper = Mage::helper('core/url');
+
+    $url = $helper->getCurrentUrl();
+    $url = $helper->addRequestParam($url, array('rss' => 1));
+
+    $head->addItem('rss', $url, 'title="' . $head->getTitle() . '"');
+  }
 }
