d2debb631b31ee3113f1a794e32f5df0a9f00119
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-02-04 12:44:07 +0000

    (Productivity) Add block to show content of feed's item (issue #390)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1228 805d05ed-e12b-7278-cd1c-88623764219b

---
+++ app/code/local/ZetaPrints/MvProductivityPack/Block/Rss/Import.php
@@ -0,0 +1,58 @@
+<?php
+
+class ZetaPrints_MvProductivityPack_Block_Rss_import
+  extends Mage_Core_Block_Template {
+
+  public function __construct() {
+    parent::__construct();
+
+    //Default template
+    $this->setTemplate('productivity/rss/import.phtml');
+
+    //Default period
+    $this->setCacheLifetime(86400);
+  }
+
+  public function getCacheKeyInfo () {
+    $design = Mage::getDesign();
+
+    return array_merge(
+             parent::getCacheKeyInfo(),
+             array(
+               $this->getData('url'),
+               $design->getTheme('layout'),
+               $design->getTheme('locale')
+             )
+           );
+  }
+
+  public function getFeed () {
+    $feed = $this->getData('feed');
+
+    if ($feed)
+      return $feed;
+
+    $uri = $this->getUri();
+
+    if (!$uri)
+      return null;
+
+    $feed = Zend_Feed::import($uri);
+
+    $this->setFeed($feed);
+
+    return $feed;
+  }
+
+  public function getContent () {
+    if (!$feed = $this->getFeed())
+      return false;
+
+    if (!count($feed))
+      return false;
+
+    return $feed
+             ->current()
+             ->encoded();
+  }
+}
