1745e78ad8409164d0e9d371e25f5d7808a0ce0f
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-03-28 08:25:22 +0000

    (Productivity) Update code for top panel; change panel look (issue #420)
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1457 805d05ed-e12b-7278-cd1c-88623764219b

--- app/code/local/ZetaPrints/MvProductivityPack/Block/Panel.php
+++ app/code/local/ZetaPrints/MvProductivityPack/Block/Panel.php
@@ -2,10 +2,43 @@
 /**
  * Block for frontend top panel
  *
- * @category   ZetaPrints_MvProductivityPack_Block_Panel
- * @package    ZetaPrints_MvProductivityPack
- * @author     ZetaPrints <anemets1@gmail.com>
- */ 
-class ZetaPrints_MvProductivityPack_Block_Panel extends Mage_Core_Block_Template {
-   
+ * @category ZetaPrints_MvProductivityPack_Block_Panel
+ * @package  ZetaPrints_MvProductivityPack
+ * @author ZetaPrints
+ */
+class ZetaPrints_MvProductivityPack_Block_Panel
+  extends Mage_Core_Block_Template {
+
+  protected function _getType () {
+    return $this->getRequest()->getControllerName();
+  }
+
+  protected function _getProductLink () {
+    $params['id'] = Mage::registry('product')->getId();
+
+    return Mage::helper('adminhtml')
+             ->getUrl('adminhtml/catalog_product/edit/', $params);
+  }
+
+  protected function _getCmsPageLink () {
+    $page = $this->getHelper('cms/page')->getPage();
+
+    if (!$pageId = $page->getPageId())
+      return false;
+
+    $params['page_id'] = $pageId;
+
+    return Mage::helper('adminhtml')
+             ->getUrl('mvproductivitypack/adminhtml_index/index/', $params);
+  }
+
+  protected function _getCategoryLink () {
+    if (!$category = Mage::registry('current_category'))
+      return false;
+
+    $params['id'] = $category->getId();
+
+    return Mage::helper('adminhtml')
+             ->getUrl('adminhtml/catalog_category/edit/', $params);
+  }
 }
