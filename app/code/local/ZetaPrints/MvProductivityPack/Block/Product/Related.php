95109b401de0d050aa0d543a26b6552629a5cba6
Anatoly.Kazantsev@gmail.com <Anatoly.Kazantsev@gmail.com@805d05ed-e12b-7278-cd1c-88623764219b>
2013-11-28 14:42:43 +0000

    (Productivity) Issue #645: implement related products block
    git-svn-id: https://mageventory.googlecode.com/svn/branches@1963 805d05ed-e12b-7278-cd1c-88623764219b

---
+++ app/code/local/ZetaPrints/MvProductivityPack/Block/Product/Related.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Related products block
+ *
+ * @category ZetaPrints_MvProductivityPack_Block_Product_Latest
+ * @package  ZetaPrints_MvProductivityPack
+ * @author ZetaPrints
+ */
+class ZetaPrints_MvProductivityPack_Block_Product_Related
+  extends Mage_Catalog_Block_Product_Abstract {
+
+  public function getCacheKeyInfo () {
+    return array_merge(
+      parent::getCacheKeyInfo(),
+      array(
+        Mage::getSingleton('customer/session')->getCustomerGroupId(),
+        ($product = $this->getProduct()) ? $product->getId() : null,
+        $this->getData('attribute_code'),
+        $this->getData('product_count')
+      )
+    );
+  }
+
+  public function getProductCollection () {
+    $collection = $this->getData('product_collection');
+
+    if ($collection)
+      return $collection;
+
+    $this->setData(
+      'product_collection',
+      $collection = Mage::getResourceModel('catalog/product_collection')
+    );
+
+    if (!(($product = $this->getProduct())
+          && ($productId = $product->getId())))
+      return $collection;
+
+    if (!$code = trim($this->getData('attribute_code')))
+      return $collection;
+
+    if (($value = $product->getData($code)) === null)
+      return $collection;
+
+    $visibility = Mage::getSingleton('catalog/product_visibility')
+                    ->getVisibleInCatalogIds();
+
+    $imageFilter = array('nin' => array('no_selection', ''));
+
+    $collection
+      ->addAttributeToSelect(array(
+          'name',
+          'special_price',
+          'special_from_date',
+          'special_to_date'
+        ))
+      ->addPriceData()
+      ->addUrlRewrite()
+      ->addIdFilter($productId, true)
+      ->addAttributeToFilter($code, $value)
+      ->addAttributeToFilter('small_image', $imageFilter)
+      ->setVisibility($visibility)
+      ->addStoreFilter()
+      ->setPageSize($this->getProductsCount())
+      ->setCurPage(1);
+
+    $collection
+      ->getSelect()
+      ->order(new Zend_Db_Expr('RAND()'));
+
+    return $collection;
+  }
+}
